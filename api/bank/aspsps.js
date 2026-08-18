80dd8723eb68489878162751a96e00f80da6b19c
avrumig16-blip <avrumig16@gmail.com>
2026-08-18 12:35:47 +0200

    Delete api/bank directory

--- api/bank/aspsps.js
+++
@@ -1,2 +0,0 @@
-const {eb}=require('./_lib');
-module.exports=async(req,res)=>{try{const country=String(req.query.country||'BE').toUpperCase(),psu=String(req.query.psu_type||'business');const q=new URLSearchParams({country,psu_type:psu,service:'AIS'});const data=await eb(`/aspsps?${q.toString()}`);res.status(200).json({aspsps:data.aspsps||[]})}catch(e){res.status(500).json({error:e.message})}};
